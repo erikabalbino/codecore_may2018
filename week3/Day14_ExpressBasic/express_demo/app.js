8bcdebdbfdd248c9fec55eb1b8402217e9537782
Erika Balbino <erk.balbino@gmail.com>
2018-06-14 11:53:01 -0700

    Add survey views and getting query results

--- week3/Day14_ExpressBasic/express_demo/app.js
+++ week3/Day14_ExpressBasic/express_demo/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+// This configures our express app to "ejs" to
+// render its views (the HTML it responds with.)
+app.set("view engine", "ejs");
+
 // Calling "morgan" returns a middleware function that
 // we pass as a argument to app.use(). Everytime
 // a request will be made to our this middleware function
@@ -39,7 +43,61 @@ app.get("/hello_world", (request, response) => {
     // of a express app we will build the response then
     // finally send it.
     response.send("Hello, World!");
-  });
+});
+
+app.get("/", (request, response) => {
+    // `response.render(<ejs-filename>)` is used to
+    // render an template from the "views/" directory.
+    // Replace <ejs-filename> with a path to a file
+    // beginning after the "views/" directory ignoring
+    // the file extension.
+
+    // The call below is looking for a file at
+    // `./views/welcome.ejs`
+    response.render("Welcome")
+});
+
+// "/survey" is the URL path to get this route.
+app.get("/survey", (request, response) => {
+    // To make use of query string data, access with the query
+    // property on the request object. Express will have converted to
+    // a JavaScript object.
+    console.log("Query string:", request.query);
+    // "survey" in render() is the EXACT name of file inside of
+    // "views/" without the extension (i.e. .ejs).
+    response.render("survey");
+});
+
+// .../survey?fullName=Steve&timeOfDay=Morning&colour=%23942192
+//     path  | query string
+
+// A "query" is a way to represent in a url using the
+// URL Encoding format.
+// Following a url's path a query beings with "?".
+// It is followed by key's and value's that correspong to
+// the input's name and value respectively when form is submitted.
+// ?[key]=[value]&[key]=[value]&[key]=[value]
+
+app.get("/survey/results", (request, response) => {
+    // Use response.send() with variables to check to display in the
+    // browser as JSON. Here we use to display the contents
+    // of `request.query`.
+    // response.send(request.query);
+  
+    const fullName = request.query.fullName;
+    const timeOfDay = request.query.timeOfDay;
+    const color = request.query.color;
+  
+    // (optional) `response.render` can take an object as
+    // a second argument. All property-value pairs of that
+    // object will become variables named after the property
+    // inside of the template's ejs file.
+    response.render("surveyResults", {
+      fullName: fullName,
+      timeOfDay: timeOfDay,
+      color: color
+    });
+});
 
 const PORT = 4545;
 const DOMAIN = "localhost"; //127.0.0.1
