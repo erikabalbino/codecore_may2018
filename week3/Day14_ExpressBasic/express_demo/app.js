13be06b42b51a4b8cfd9d3f3f9f7aeb24cec9ef3
Erika Balbino <erk.balbino@gmail.com>
2018-06-14 10:20:14 -0700

    Add and install nodemon

--- week3/Day14_ExpressBasic/express_demo/app.js
+++ week3/Day14_ExpressBasic/express_demo/app.js
@@ -1,9 +1,38 @@
 const express = require("express");
 const app = express();
 
+// URL http://localhost:4545/hello_world
+// scheme  | host     | port | path
+
+// The "scheme" identifies the protocol being used.
+// Typically, HTTP or HTTPS, but could be SSH, FTP, TCP, etc.
+
+// The "host" identifies the domain or IP that is running
+// the server.
+
+// The "path" which identifies the specific resource
+// a client makes a request to.
+
+// The following method creates a "route" wich a callback
+// that responds to a request.
+// The tells our app to respond to request at the path
+// "/hello_world" with the "Hello, World!".
+
+// The first argument of app.get() is a path written as
+// a string. The second argument is called that must
+// complete a response to the client.
 app.get("/hello_world", (request, response) => {
+    // The "request" argument is an object that represents
+    // the request being made by the client. It contains
+    // information about the client and everything the client
+    // is asking for.
+  
+    // The "response" argument is an object that represents
+    // the server's reply to the client. Through the course
+    // of a express app we will build the response then
+    // finally send it.
     response.send("Hello, World!");
-});
+  });
 
 const PORT = 4545;
 const DOMAIN = "localhost"; //127.0.0.1
