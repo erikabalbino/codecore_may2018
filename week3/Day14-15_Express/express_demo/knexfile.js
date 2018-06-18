294e5a8f8ff129644f367dc378499f3e476b8180
Erika Balbino <erk.balbino@gmail.com>
2018-06-18 10:26:15 -0700

    Prepare knexfile.js then add db:create & db:drop scripts

--- week3/Day14-15_Express/express_demo/knexfile.js
+++ week3/Day14-15_Express/express_demo/knexfile.js
@@ -3,42 +3,13 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      database: "express_demo"
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tabeName: "Knex_migrations",
+      directory: "./db/migrations"
     }
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
