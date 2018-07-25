332bebdb9cf92180c22cd5ed5f6a7ce1a4d87f92
Erika Balbino <erk.balbino@gmail.com>
2018-07-25 11:06:35 -0700

    Add demo for Breakup Question Details

--- JSReact/awesome-answers-react/src/index.js
+++ JSReact/awesome-answers-react/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+// When writing components, you named with beginning with a capital
+// letter. Otherwise, React will interpret your component as an
+// HTML tag and will use your component function.
+
+const Field = props => {
+    return (
+      <small>
+        <strong>{props.name}: </strong>
+        {props.value}
+      </small>
+    );
+  };
+
 // A function that returns a React Element (the value returned
 // by a call from React.createElement(...)) is a React Component.
 
@@ -13,18 +26,12 @@ const QuestionDetails = props => {
             <p>{props.body}</p>
             <p>By {props.author.full_name}</p>
             <p>
-                <small>
-                    <strong>View Count: </strong>
-                    {props.view_count}
-                </small>{" • "} 
-                <small>
-                    <strong>Created at: </strong>
-                    {props.created_at.toLocaleString()} 
-                </small>{" • "} 
-                <small>
-                    <strong>Updated at: </strong>
-                    {props.updated_at.toLocaleString()} 
-                </small>{" • "} 
+                <Field name="View Count" value={props.view_count} />
+                {" • "} 
+                <Field name="Created At" value={props.created_at.toLocaleString()} />
+                {" • "} 
+                <Field name="Updated At" value={props.updated_at.toLocaleString()} />
+                {" • "} 
             </p>
         </div>
     );
@@ -36,10 +43,7 @@ const AnswerDetails = props => {
             <h2>Answers</h2>
             <p>{props.body}</p>
             <strong> By {props.author.full_name}</strong> <br/>
-            <small>
-                <strong>Created at: </strong>
-                {props.created_at.toLocaleString()} 
-            </small>
+            <Field name="Created At" value={props.created_at.toLocaleString()} />
         </div>
     );
 };
