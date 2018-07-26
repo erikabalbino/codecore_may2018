e53a666af25cb324261f421fb265c9886149eb1e
Erika Balbino <erk.balbino@gmail.com>
2018-07-26 10:12:13 -0700

    Add demo for Using State and Add solution to State for Index

--- JSReact/awesome-answers-react/src/components/QuestionIndexPage.jsx
+++ JSReact/awesome-answers-react/src/components/QuestionIndexPage.jsx
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
 
 class QuestionIndexPage extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            questions: props.questions || []
+        }
+
+    }
     render () {
         // When destructuring a property from an object, you
         // can assign a default value if that property is
         // "undefined".
-        const { questions = [] } = this.props;
+        const { questions } = this.state;
     
         return (
             <main>
                 <h1>Question Index</h1>
                 <ul style={{ padding: 0, listStyle: "none" }}>
-                    {this.props.questions.map((question, index) => 
+                    {questions.map((question, index) => 
+
                     <li key={question.id}>
                         <span>{new Date(question.created_at).toLocaleDateString()}</span> •{" "}
                         <a href="#">{question.title}</a>
                     </li> )}
+
                 </ul>
             </main>
         )
