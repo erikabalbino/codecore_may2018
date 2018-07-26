d77c17a5de6dfd96bedd468c492054552d7ef9b1
Erika Balbino <erk.balbino@gmail.com>
2018-07-26 09:26:22 -0700

    Add demo for Question Show to Class

--- JSReact/awesome-answers-react/src/components/QuestionShowPage.jsx
+++ JSReact/awesome-answers-react/src/components/QuestionShowPage.jsx
@@ -1,28 +1,39 @@
-import React from "react";
+
+// `Component` is a named export. Unlike `React` which is a default
+// export, a named export must written with exact names.
+
+// The two exports below are named exports where the name of the
+// variable or class is significant.
+// export const Component = { ... }
+// export class Component { ... }
+
+import React, { Component } from "react";
 import QuestionDetails from "./QuestionDetails";
 import AnswerDetails from "./AnswerDetails";
 import AnswerList from "./AnswerList";
 
-const QuestionShowPage = props => {
-  const { question } = props;
-
-  return (
-    <main>
-
-      <QuestionDetails
-        title="What is your favourite colour?"
-        body="Red, green, blue, light golden rod, lime, magenta, etc."
-        author={{ full_name: "Bridge Troll" }}
-        view_count={22}
-        created_at={new Date()}
-        updated_at={new Date()}
-      />
-
-      <h2>Answers</h2>
-      <AnswerList answers={question.answers} />
+class QuestionShowPage extends Component {
+    render () {
+        const { question } = this.props;
       
-    </main>
-  );
+        return (
+          <main>
+      
+            <QuestionDetails
+              title="What is your favourite colour?"
+              body="Red, green, blue, light golden rod, lime, magenta, etc."
+              author={{ full_name: "Bridge Troll" }}
+              view_count={22}
+              created_at={new Date()}
+              updated_at={new Date()}
+            />
+      
+            <h2>Answers</h2>
+            <AnswerList answers={question.answers} />
+      
+          </main>
+        );
+    }
 };
 
 export default QuestionShowPage;
